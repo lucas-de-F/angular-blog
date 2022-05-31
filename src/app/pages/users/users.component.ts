9ed69adaa6f446944e72d8c32aee5d1271b07e3f
lucas-de-f <francalucas937@gmail.com>
2022-05-31 12:32:09 -0300

    feat: user component

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+  users: any;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.getUser()
   }
-
+  getUser() {
+      this.userService.getUser().subscribe(
+        (res: any) => this.users = res,
+        (err: any) => console.log(err)
+      );
+  }
 }
