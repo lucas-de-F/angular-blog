b688474a4eea0ebea379a6a4c89795de6182ae2b
lucas-de-f <francalucas937@gmail.com>
2022-05-31 10:54:22 -0300

    feat: register page

--- src/app/home/register/register.component.ts
+++ src/app/home/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  email = new FormControl('', [
+    Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
+  ]);
+  password = new FormControl('', [
+    Validators.minLength(6)
+  ]);
+  confirmPassword = new FormControl('', [
+    Validators.minLength(6),
+  ]);
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  getErrorMessage(str: string) {
+    console.log(`${str} INVÁLIDO`)
+  }
+
+  getSuccessMessage(str: string) {
+    console.log(`${str} VÁLIDO`)
+  }
+
+  redirect() {
+    this.router.navigateByUrl('/posts')
+  }
+
+  login() {
+    this.router.navigateByUrl('/login')
+  }
 }
