9ac9e43559d504855157cb19c4dd75caf7c39fa1
lucas-de-f <francalucas937@gmail.com>
2022-06-02 10:41:13 -0300

    feat: jwt decode

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user/user.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
+interface User {
+  email: string;
+  password: string
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  email = new FormControl('', [
+  email = new FormControl('lewishamilton@gmail.com', [
     Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
   ]);
   password = new FormControl('', [
     Validators.minLength(6)
   ]);
-
-  constructor(private router: Router) { }
+  private jwtItils = new JwtHelperService()
+  constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit(): void {
   }
@@ -31,5 +37,26 @@ export class LoginComponent implements OnInit {
 
   register() {
     this.router.navigateByUrl('/register')
+  }
+  public getToken(): string {
+    return localStorage.getItem('token') as string;
+  }
+
+  public decodePayloadJWT(): any {
+    try {
+      return console.log(this.jwtItils.decodeToken(this.getToken()));
+    } catch (Error) {
+      return null;
+    }
+  }
+  login(): any {
+    const data: User = { email: this.email.value, password: this.password.value }
+    this.userService.UserLogin(data).subscribe(
+      (res: any) => {
+        localStorage.setItem('token', res.token)
+        this.decodePayloadJWT()
+      },
+      (err: any) => {console.log(err)}
+    );
   }
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -12,4 +12,8 @@ export class UserService {
   getUser(): Observable<any> {
     return this.http.get('http://localhost:3000/user');
   }
+
+  UserLogin({ email, password }: { email: string, password: string }): Observable<any> {
+    return this.http.post('http://localhost:3000/login', { email, password });
+  }
 }
