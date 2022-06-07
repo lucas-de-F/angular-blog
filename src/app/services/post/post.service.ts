57380d6a9f7bafcaab8262aaaa7a769d0bbbee29
lucas-de-f <francalucas937@gmail.com>
2022-06-07 09:40:22 -0300

    feat: add token header

--- src/app/services/post/post.service.ts
+++ src/app/services/post/post.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -9,7 +10,14 @@ export class PostService {
 
   constructor(private http: HttpClient) { }
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      Authorization: `${localStorage.getItem('token')}`
+    })
+  };
+
   getPosts(): Observable<any> {
-    return this.http.get('http://localhost:3000/post');
+    return this.http.get('http://localhost:3000/post', this.httpOptions);
   }
 }
