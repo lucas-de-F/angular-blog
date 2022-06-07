efee36cbb818e43c7945f4c610636bc7cdeaed5c
lucas-de-f <francalucas937@gmail.com>
2022-06-07 10:48:11 -0300

    feat: front end register implemented

--- src/app/home/register/register.component.ts
+++ src/app/home/register/register.component.ts
@@ -1,13 +1,20 @@
+import { UserService } from 'src/app/services/user/user.service';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
+interface User {
+  email: string;
+  password: string
+}
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
+
 export class RegisterComponent implements OnInit {
+  error = ''
   email = new FormControl('', [
     Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
   ]);
@@ -18,19 +25,28 @@ export class RegisterComponent implements OnInit {
     Validators.minLength(6),
   ]);
 
-  constructor(private router: Router) { }
-
+  constructor(
+    private router: Router,
+    private userService: UserService,
+  ) { }
   ngOnInit(): void {
-  }
-
-  getErrorMessage(str: string) {
-  }
-
-  getSuccessMessage(str: string) {
   }
 
   redirect() {
     this.router.navigateByUrl('/posts')
+  }
+
+  register(): any {
+    const data: User = { email: this.email.value, password: this.password.value }
+    this.userService.UserRegister(data).subscribe(
+      (res: any) => {
+        localStorage.setItem('token', res.token)
+        this.redirect()
+      },
+      (err: any) => {
+        this.error = err.error.message
+      }
+    );
   }
 
   login() {
