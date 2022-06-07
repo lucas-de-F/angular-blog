1804d640e2e4e36a5407365679d17970a5022f92
lucas-de-f <francalucas937@gmail.com>
2022-06-07 09:40:34 -0300

    feat: add token header

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +10,22 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      Authorization: `${localStorage.getItem('token')}`
+    })
+  };
+
   getUser(): Observable<any> {
-    return this.http.get('http://localhost:3000/user');
+    return this.http.get('http://localhost:3000/user', this.httpOptions);
   }
 
   getUserById(id: string) {
-    return this.http.get(`http://localhost:3000/user/${id}`);
+    return this.http.get(`http://localhost:3000/user/${id}`, this.httpOptions);
   }
 
   UserLogin({ email, password }: { email: string, password: string }): Observable<any> {
-    return this.http.post('http://localhost:3000/login', { email, password });
+    return this.http.post('http://localhost:3000/login', { email, password },  this.httpOptions);
   }
 }
